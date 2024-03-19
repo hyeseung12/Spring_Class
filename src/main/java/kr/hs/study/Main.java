ec866a067369bb07eb85cdc31bba2f2b157a61be
MsEmily1020 <peace10200khs@gmail.com>
2024-03-19 15:02:13 +0900

    03-19 :: constructor DI 기본형

--- src/main/java/kr/hs/study/Main.java
+++ src/main/java/kr/hs/study/Main.java
@@ -7,8 +7,25 @@ public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("config.xml");
 
-        TestBean t1 = ctx.getBean("t1", TestBean.class);
-        t1.prData();
+        TestBean obj1 = ctx.getBean("t1", TestBean.class);
+        obj1.prData();
+
+        TestBean obj2 = ctx.getBean("t2", TestBean.class);
+        obj2.prData();
+
+        TestBean obj3 = ctx.getBean("t3", TestBean.class);
+        obj3.prData();
+
+        TestBean obj4 = ctx.getBean("t4", TestBean.class);
+        obj4.prData();
+
+        // java 코드로 AllArgsConstructor 값 출력하기
+        TestBean java = new TestBean(66, 66.66, "Mike66");
+        // TestBean java = new TestBean("Mike66", 66.66, 66); -> java 에서는 타입 순서 중요함 (에러 발생)
+        java.prData();
+
+        TestBean obj5 = ctx.getBean("t5", TestBean.class);
+        obj5.prData();
 
         ctx.close();
     }
--- src/main/java/kr/hs/study/beans/TestBean.java
+++ src/main/java/kr/hs/study/beans/TestBean.java
@@ -6,14 +6,44 @@ public class TestBean {
     String data3;
 
     public TestBean() {
+        System.out.println("TestBean의 기본생성자");
         this.data1 = 11;
         this.data2 = 11.11;
         this.data3 = "Mike11";
     }
 
+    public TestBean(int data1) {
+        System.out.println("obj2: int 받는 생성자");
+        this.data1 = data1;
+        this.data2 = 22.22;
+        this.data3 = "Mike22";
+    }
+
+    public TestBean(double data2) {
+        System.out.println("obj3 : double 받는 생성자");
+        this.data1 = 33;
+        this.data2 = data2;
+        this.data3 = "Mike33";
+    }
+
+    public TestBean(String data3) {
+        System.out.println("obj4 : string 받는 생성자");
+        this.data1 = 44;
+        this.data2 = 44.44;
+        this.data3 = data3;
+    }
+
+    public TestBean(int data1, double data2, String data3) {
+        this.data1 = data1;
+        this.data2 = data2;
+        this.data3 = data3;
+    }
+
     public void prData() {
+        System.out.println("================================================");
         System.out.println("data1 : " + data1);
         System.out.println("data2 : " + data2);
         System.out.println("data3 : " + data3);
+        System.out.println("================================================");
     }
 }
